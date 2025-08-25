504e76b8f636aac89821a0b26950897dc4d22a00
Dohyeon Won <dh1072005@gmail.com>
2025-08-25 22:13:25 +0900

    [DESIGN] 메인페이지

--- ui/src/text/MainPage.ts
+++ ui/src/text/MainPage.ts
@@ -8,13 +8,13 @@ export const mainPageTexts: Record<"ENG" | "KOR", MainPageTexts> = {
     ENG: {
         description:
             "Your smart planner that adapts to you. Just speak, and your plan is complete.Plan your day and share your achievements with the world.",
-        learnMoreButton: "Learn More About Silhouette",
+        learnMoreButton: "Learn More",
         signInButton: "Sign in with Google",
     },
     KOR: {
         description:
             "당신에게 맞춰지는 스마트 플래너. 말로 하면 계획이 완성돼요. 일상을 계획하고, 당신의 성취를 세상과 공유하세요.",
-        learnMoreButton: "Silhouette에 대해 더 알아보기",
+        learnMoreButton: "더 알아보기",
         signInButton: "구글 계정으로 로그인",
     },
 };
